3fbc71d2e83f07d9bf5132a10a4e7ba971a969d5
Ed Kellett <e@kellett.im>
2020-10-22 22:56:31 +0100

    Add STAG server cap

--- include/s_serv.h
+++ include/s_serv.h
@@ -92,6 +92,7 @@ extern unsigned int CAP_EUID;			/* supports EUID (ext UID + nonencap CHGHOST) */
 extern unsigned int CAP_EOPMOD;			/* supports EOPMOD (ext +z + ext topic) */
 extern unsigned int CAP_BAN;			/* supports propagated bans */
 extern unsigned int CAP_MLOCK;			/* supports MLOCK messages */
+extern unsigned int CAP_STAG;			/* supports s2s tags and labeled-response */
 
 /* XXX: added for backwards compatibility. --nenolod */
 #define CAP_MASK	(capability_index_mask(serv_capindex) & ~(CAP_TS6 | CAP_CAP))
