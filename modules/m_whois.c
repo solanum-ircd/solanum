69117f4802d234cf9f7a3225e691b32eebe42448
Aaron Jones <me@aaronmdjones.net>
2022-09-26 07:13:06 +0000

    extensions/umode_hide_idle_time: mask times for hidden sources
    If the user performing a WHOIS has hidden their idle time (is umode +I),
    also prevent them from seeing other user's idle times, even if said users
    are not themselves umode +I.
    
    Suggested-by: @Unit193

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -30,10 +30,10 @@
 #include "ircd.h"
 #include "numeric.h"
 #include "s_conf.h"
+#include "s_user.h"
 #include "s_serv.h"
 #include "send.h"
 #include "match.h"
-#include "s_conf.h"
 #include "logger.h"
 #include "msg.h"
 #include "parse.h"
@@ -389,9 +389,12 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 			   hdata_showidle.approved ? (long)(rb_current_time() - target_p->localClient->last) : 0,
 			   (unsigned long)target_p->localClient->firsttime);
 
-		if (hdata_showidle.approved == WHOIS_IDLE_AUSPEX || hdata_showidle.approved == WHOIS_IDLE_HIDE)
+		if (hdata_showidle.approved == WHOIS_IDLE_HIDE && (source_p->umodes & user_modes['I']))
+			/* if the source has hidden their idle time, notify the source that they can't view others' idle times either */
+			sendto_one_numeric(source_p, RPL_WHOISTEXT, form_str(RPL_WHOISTEXT), target_p->name, "has a hidden idle time because your own idle time is hidden");
+		else if (hdata_showidle.approved != WHOIS_IDLE_SHOW)
 			/* if the target has hidden their idle time, notify the source */
-			sendto_one_numeric(source_p, RPL_WHOISTEXT, form_str(RPL_WHOISTEXT), target_p->name, "is using a private idle time");
+			sendto_one_numeric(source_p, RPL_WHOISTEXT, form_str(RPL_WHOISTEXT), target_p->name, "is hiding their idle time");
 	}
 	else
 	{
