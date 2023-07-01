f561a8f59897449a54411255f45d339d0e82d598
Aaron Jones <me@aaronmdjones.net>
2023-07-01 18:02:54 +0000

    extensions/extb_guest: support CIDR masks in $g extbans
    This allows a channel operator to set a channel ban such as
    "$g:*!*@192.0.2.0/24#*web.libera.chat*" and have it function
    as intended.

--- extensions/extb_guest.c
+++ extensions/extb_guest.c
@@ -33,11 +33,41 @@ _moddeinit(void)
 static int eb_guest(const char *data, struct Client *client_p,
 		struct Channel *chptr, long mode_type)
 {
+	(void)chptr;
+
 	if (data == NULL)
+		return EXTBAN_INVALID;
+
+	const char *idx = strchr(data, '#');
+
+	if (idx != NULL && idx[1] == '\0')
+		/* Users cannot have empty realnames,
+		 * so don't let a ban be set matching one
+		 */
 		return EXTBAN_INVALID;
 
 	if (!EmptyString(client_p->user->suser))
 		return EXTBAN_NOMATCH;
 
-	return client_matches_mask(client_p, data) ? EXTBAN_MATCH : EXTBAN_NOMATCH;
+	if (idx != NULL)
+	{
+		char buf[BUFSIZE];
+
+		// Copy the nick!user@host part of the ban
+		memcpy(buf, data, (idx - data));
+		buf[(idx - data)] = '\0';
+
+		// Advance to the realname part of the ban
+		idx++;
+
+		if (client_matches_mask(client_p, buf) && match(idx, client_p->info))
+			return EXTBAN_MATCH;
+
+		return EXTBAN_NOMATCH;
+	}
+
+	if (client_matches_mask(client_p, data))
+		return EXTBAN_MATCH;
+
+	return EXTBAN_NOMATCH;
 }
